490902ec971d1693c1f8e7d86d06849fc0336bc5
johnwinstondarby <johnwinstondarby@gmail.com>
2026-08-22 11:08:58 -0700

    Add GPL-3.0-or-later notices to source files

--- canary/verify/VerifySupplementalIDML108.py
+++ canary/verify/VerifySupplementalIDML108.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+# StyleFix - character style auditing for Adobe InDesign documents
+# Copyright (C) 2026 John Darby
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 """Independent IDML verifier for the StyleFix v1.0.8 supplemental fixture.
 
 The verifier reads the IDML ZIP directly and shares no ExtendScript traversal
